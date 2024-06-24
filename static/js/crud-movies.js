bd8379f567dc0063bed8565c32861e3aab7f9d4c
fom78Dev <fom78a@gmail.com>
2024-06-24 19:32:22 -0300

    clase 30 inicial

--- static/js/crud-movies.js
+++ static/js/crud-movies.js
@@ -2,23 +2,35 @@ import { API_MOVIES } from './config.js';
 document.getElementById('movieForm').addEventListener('submit', function(event) {
   event.preventDefault();
   
-  const title = document.getElementById('title').value;
-  const release_year = document.getElementById('release_year').value;
-  const adult = document.getElementById('adult').checked ? 1 : 0;
+  const title = document.getElementById('title').value
+  const release_year = document.getElementById('release_year').value
+  const adult = document.getElementById('adult').checked ? 1 : 0
 
-  const data = {
-      title: title,
-      release_year: parseInt(release_year),
-      adult: adult,
-      poster_url: ""
-  };
+//   obtenemos la imagen que vamos a subir  
+// obtener el primer archivo seleccionado en un campo de entrada de tipo file en un formulario HTML
+const imagen = document.getElementById('imagenPeli').files[0]
+console.log("IMAGEN", imagen, "TIPO: ", typeof(imagen))
 
-  fetch(API_MOVIES, {
+
+
+
+//   FormData (formData):
+
+//   Es una interfaz específica de JavaScript para construir un conjunto de pares clave/valor 
+//   que pueden incluir no solo datos simples sino también archivos binarios.
+
+//   FormData es la elección correcta para enviar datos de formularios que incluyen 
+//   archivos a través de una solicitud HTTP.
+
+let formData = new FormData()
+formData.append('title',title)
+formData.append('release_year',release_year)
+formData.append('adult',adult)
+formData.append('imagen',imagen)
+
+  fetch(`${API_MOVIES}/movies1`, {
       method: 'POST',
-      headers: {
-          'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
+      body: formData // Aquí enviamos formData en lugar de JSON
   })
   .then(response => response.json())
   .then(data => {
--- static/js/listado.js
+++ static/js/listado.js
@@ -1,18 +1,17 @@
-// const API_MOVIES = 'https://api.themoviedb.org/3/movie/popular'
-const API_MOVIES = 'http://localhost:5000/movies'
+
+import { API_MOVIES } from './config.js'
+
 
 
 const options = {
     method: 'GET',
     headers: {
-        accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2MTI2ZDgzMDU2NjMzNmJhNmU4Mzc2NGIyZjZiZmI2MSIsInN1YiI6IjY1Y2U2NDA0MTNhMzg4MDE4NzlmNjBmOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.tZTxSjr1fLqIi2LSwadmPT37grY2IF6y3d4LUHLbEmE'
-    }
+        accept: 'application/json'    }
 }
 
 const obtenerPelis = async () => {
 
-    const resultado = await fetch(API_MOVIES, options)
+    const resultado = await fetch(`${API_MOVIES}/movies`, options)
     const data = await resultado.json()
 
     // const pelis = data.results
@@ -27,7 +26,7 @@ const obtenerPelis = async () => {
         const PeliAInsertar =`
         <div class="movie-item">
                     <a href="./templates/detail-movie.html" target="_blank">
-                        <img src="https://image.tmdb.org/t/p/w500/nP6RliHjxsz4irTKsxe8FRhKZYl.jpg" width="250" alt="" class="movie-item-img">
+                        <img src="${API_MOVIES}/static/img/${pelis[i].poster_url}" width="250" alt="" class="movie-item-img">
                         <div class="movie-item-detail">
                             <p class="movie-item-detail-title">${pelis[i].title}</p>
                             <p class="movie-item-detail-subtitle">${pelis[i].release_year}</p>
@@ -44,30 +43,3 @@ const obtenerPelis = async () => {
 
 
 obtenerPelis()
-
-/*
-const agregarPeli = async (newMovie) => {
-    const response = await fetch(API_MOVIES, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            accept: 'application/json',
-            
-        },
-        body: JSON.stringify(newMovie)
-    });
-
-    const data = await response.json();
-    console.log('New Movie Added:', data);
-};
-
-// Ejemplo de cómo llamar a agregarPeli con una nueva película
-const nuevaPeli = {
-    id:120,
-    title: "Test",
-    release_year: 2010,
-    adult: false
-};
-
-agregarPeli(nuevaPeli); // Llama para agregar una nueva película
-*/
